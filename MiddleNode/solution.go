ceda9a2d467929807c864046eedd5bd991d7236e
Nofre <davidprogram417@gmail.com>
2024-12-05 20:26:52 +0100

    added find middle node algorithm

---
+++ MiddleNode/solution.go
@@ -0,0 +1,26 @@
+package MiddleNode
+
+import . "algoexpert/models"
+
+func MiddleNode(head *LinkedList[int]) *LinkedList[int] {
+	var resultNode, current *LinkedList[int]
+	var nodeMap = make(map[int]*LinkedList[int])
+	var idx int
+	current = head
+
+	for current != nil {
+		node := current
+		nodeMap[idx] = node
+		current = current.Next
+		idx++
+	}
+
+	length := len(nodeMap)
+	if length%2 == 0 {
+		resultNode = nodeMap[length/2]
+	} else {
+		resultNode = nodeMap[(length/2)-1]
+	}
+
+	return resultNode
+}
---
+++ MiddleNode/solution_test.go
@@ -0,0 +1,52 @@
+package MiddleNode
+
+import (
+	. "algoexpert/models"
+	"reflect"
+	"testing"
+)
+
+func Test_MiddleNode(t *testing.T) {
+	testFunctions := map[string]func(head *LinkedList[int]) *LinkedList[int]{
+		"MiddleNode_V1": MiddleNode,
+	}
+
+	type args struct {
+		head *LinkedList[int]
+	}
+
+	tests := []struct {
+		args args
+		want *LinkedList[int]
+	}{
+		{
+			args: args{
+				head: &LinkedList[int]{
+					Value: 2,
+					Next: &LinkedList[int]{
+						Value: 7,
+						Next: &LinkedList[int]{
+							Value: 3,
+							Next: &LinkedList[int]{
+								Value: 5,
+							},
+						},
+					},
+				},
+			},
+			want: &LinkedList[int]{
+				Value: 3,
+			},
+		},
+	}
+
+	for name, fn := range testFunctions {
+		t.Run(name, func(t *testing.T) {
+			for _, tt := range tests {
+				if got := fn(tt.args.head); !reflect.DeepEqual(got.Value, tt.want.Value) {
+					t.Errorf("%s = %v, want %v", name, got, tt.want)
+				}
+			}
+		})
+	}
+}
